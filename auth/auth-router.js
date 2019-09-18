f3cf3f318d79a95407f7a346b2143645bbdc7078
Ben <allenben@protonmail.com>
2019-09-18 11:29:27 -0500

    Added generate token func

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const Users = require('../users/users-model.js');
 
@@ -25,9 +26,8 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({
-          message: `Welcome ${user.username}!`,
-        });
+        const token = generateToken(user);
+        res.status(200).json({ token });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
       }
@@ -36,5 +36,18 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user){
+
+  const payload = {
+    username: user.username
+  };
+  const secret = "fdsk$%@#TFDG()F$EFWJE(T%$()W)R#@J";
+  const options = {
+    expiresIn: "4h",
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
